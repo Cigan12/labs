7de6d10cd2d72d73162721784ddd0cfb03f8e122
Cigan <n.ivanov@hookprod.com>
2020-11-18 11:25:57 +0700

    api and docker config updated

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+console.log("Cigan-log: process.env", process.env.NODE_ENV);
+
 export const API = axios.create({
-    baseURL: "http://localhost:3002/",
+    baseURL:
+        process.env.NODE_ENV === "development"
+            ? "http://localhost:3001"
+            : "https://api.olya-get-well.ru",
 });
